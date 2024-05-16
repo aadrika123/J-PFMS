b0143a0d62b6d38472ad97b2e5a43e9f07b6ad17
viizz29 <viizz29@gmail.com>
2024-05-16 06:47:24 -0400

    improvements

--- frontend/src/app/project-management/view/[id]/ProjectProposalApprovalView.tsx
+++ frontend/src/app/project-management/view/[id]/ProjectProposalApprovalView.tsx
@@ -88,7 +88,7 @@ export const Paragraph = ({ desc }: { desc: string }) => {
 
 
 const ProjectProposalApprovalView = ({ ProProposalId }: { ProProposalId: number }) => {
-  const {data: projectProposalDetails, refetch: refetchProjectProposalDetails} = useProjectProposalDetails(ProProposalId);
+  const { data: projectProposalDetails, refetch: refetchProjectProposalDetails } = useProjectProposalDetails(ProProposalId);
   const [workingAnimation, activateWorkingAnimation, hideWorkingAnimation] = useWorkingAnimation();
 
   const [projectApprovalStepper] = useProjectApprovalStepper();
@@ -124,92 +124,93 @@ const ProjectProposalApprovalView = ({ ProProposalId }: { ProProposalId: number 
 
   return (
     <>
-    {workingAnimation}
+      {workingAnimation}
       {acknowledgementPopupVisible && (<ConfirmationPopup message="Acknowledge the assignement?" cancel={() => setAcknowledgetmentPopupVisible(false)} continue={acknowledge} />)}
-      
-      
+
+
       {measurementFormVisible && (
-              <Popup width={80} zindex={30}>
-             <AddMeasurementComponent onBack={() => setMeasurementFormVisible(false)}/>
-            </Popup>
-      
+        <Popup width={80} zindex={30}>
+          <AddMeasurementComponent onBack={() => setMeasurementFormVisible(false)} />
+        </Popup>
+
       )}
 
       <div className="shadow-lg p-4 border">
-        
+
         {projectApprovalStepper}
 
         <div className="flex items-center gap-2 mt-4">
-      <div className="bg-gray-100 border flex flex-col p-4 h-52 w-1/3 items-center justify-center rounded">
-        <BoldSpan
-          className="text-secondary_black mb-4 text-center"
-          label={""}
+          <div className="bg-gray-100 border flex flex-col p-4 h-52 w-1/3 items-center justify-center rounded">
+            <BoldSpan
+              className="text-secondary_black mb-4 text-center"
+              label={""}
+            />
+            <BoldSpan label={""} />
+            <BoldSpan content="Proposal Date" />
+            <div className="flex items-center mb-2">
+              <Image src={home} alt="calender" />
+              <BoldSpan
+                className="mt-2 ml-1 text-red-500"
+                content={""}
+              />
+            </div>
+          </div>
+          <div className="bg-gray-100 border flex flex-col py-4 px-8 h-52 w-full rounded">
+            <section>
+              <Title title="Project Summary" />
+              <Paragraph desc={projectProposalDetails?.summary} />
+            </section>
+          </div>
+        </div>
+
+
+        <div className="border p-6 mt-4">
+          <section>
+            <Title title="Project Description" />
+            <Paragraph
+              desc={projectProposalDetails?.description}
+            />
+          </section>
+          <hr className="mt-3 border border-gray-400" />
+          <hr className="border border-gray-400" />
+          <div className=" grid grid-cols-4 gap-4 mt-3">
+          </div>
+        </div>
+
+        <Steps
+          handleClick={handleStepClick}
+          activeStep={activeStep}
+          className="mt-4"
         />
-        <BoldSpan label={""} />
-        <BoldSpan content="Proposal Date" />
-        <div className="flex items-center mb-2">
-          <Image src={home} alt="calender" />
-          <BoldSpan
-            className="mt-2 ml-1 text-red-500"
-            content={""}
-          />
-        </div>
-      </div>
-      <div className="bg-gray-100 border flex flex-col py-4 px-8 h-52 w-full rounded">
-        <section>
-          <Title title="Project Summary" />
-          <Paragraph desc={projectProposalDetails?.summary} />
-        </section>
-      </div>
-    </div>
-
-
-    <div className="border p-6 mt-4">
-        <section>
-          <Title title="Project Description" />
-          <Paragraph
-            desc={projectProposalDetails?.description}
-          />
-        </section>
-        <hr className="mt-3 border border-gray-400" />
-        <hr className="border border-gray-400" />
-        <div className=" grid grid-cols-4 gap-4 mt-3">
-        </div>
-      </div>
-
-      <Steps
-            handleClick={handleStepClick}
-            activeStep={activeStep}
-            className="mt-4"
-          />
 
         {activeStep === 0 ? (
-          
-          projectProposalDetails?.acknowledged? (
-            <>
-            <div className="flex gap-2 justify-end">
-              <Button variant="primary">Add BOQ</Button>
-              <Button variant="primary" onClick={() => setMeasurementFormVisible(true)}>Add Measurement</Button>
-              <Button variant="primary">Upload</Button>
-            </div>
-            </>
-          ): (
-            <>
-            <div className="flex justify-end mt-4">
-              <Button variant="primary" onClick={() => setAcknowledgetmentPopupVisible(true)}>Acknowledge</Button>
-            </div>
-            <div className="flex justify-end">
-              <span className="text-red-500">*</span>
-              Please acknowledge the form to submit measurement * other details
-            </div>
-            </>
-          )
-          
+          <>
+          </>
         ) : (
-          activeStep === 1 && (
+          activeStep === 1 ? (
             <div className="mt-4">
-                Hello
+              Hello
             </div>
+          ) : (
+              projectProposalDetails?.acknowledged? (
+              <>
+                <div className="flex gap-2 justify-end">
+                  <Button variant="primary">Add BOQ</Button>
+                  <Button variant="primary" onClick={() => setMeasurementFormVisible(true)}>Add Measurement</Button>
+                  <Button variant="primary">Upload</Button>
+                </div>
+              </>
+              ): (
+              <>
+                <div className="flex justify-end mt-4">
+                  <Button variant="primary" onClick={() => setAcknowledgetmentPopupVisible(true)}>Acknowledge</Button>
+                </div>
+                <div className="flex justify-end">
+                  <span className="text-red-500">*</span>
+                  Please acknowledge the form to submit measurement * other details
+                </div>
+              </>
+              )
           )
         )}
       </div>
